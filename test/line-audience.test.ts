59f114f65df9468bfa6386063abafb42708f57b8
Tomoya Iwata <iwata.tomoya@classmethod.jp>
2024-08-08 10:17:16 +0900

    Initial commit

---
+++ test/line-audience.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LineAudience from '../lib/line-audience-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/line-audience-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LineAudience.LineAudienceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
